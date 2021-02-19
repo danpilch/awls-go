0b5b09149469fafcbf580df4c485356165004a32
danpilch <dpilch@community.com>
2021-02-19 15:15:48 +0000

    filter searching and positional search arg

--- main.go
+++ main.go
@@ -1,19 +1,76 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"github.com/olekukonko/tablewriter"
+	"os"
+	"strings"
 
 	"github.com/alecthomas/kong"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/olekukonko/tablewriter"
 )
 
 var cli struct {
-	Search string `required short:"s" help:"EC2 Instance Name search term"`
+	Search     string `required arg help:"EC2 Instance Name search term"`
+	IpOnly     bool   `short:"i" help:"Output only Private IPs"`
+	Delimiter  string `short:"d" help:"IP delimiter" default:" "`
+	FilterType string `short:"f" help:"EC2 Filter Type (https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeInstances.html)" default:"tag:Name"`
+}
+
+func buildSearchFilter(filterName string) *ec2.DescribeInstancesInput {
+	// Define search params - only basic pattern matching supported right now
+	filter := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: aws.String(filterName),
+				Values: []*string{
+					aws.String("*" + cli.Search + "*"),
+				},
+			},
+		},
+	}
+	return filter
+}
+
+func buildPrivateIpData(result *ec2.DescribeInstancesOutput) []string {
+	var privateIps = []string{}
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			privateIps = append(privateIps, string(*instance.PrivateIpAddress))
+		}
+	}
+	return privateIps
+}
+
+func buildTableData(result *ec2.DescribeInstancesOutput) ([][]string, []string) {
+	var tbl = [][]string{}
+	var tblHeaders = []string{"Name", "PrivateIp", "State", "AZ", "InstanceId", "InstanceType", "LaunchTime"}
+
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			var nameTag string
+			for _, t := range instance.Tags {
+				if *t.Key == "Name" {
+					nameTag = *t.Value
+					break
+				}
+			}
+
+			tbl = append(tbl, []string{
+				string(nameTag),
+				string(*instance.PrivateIpAddress),
+				string(*instance.State.Name),
+				string(*instance.Placement.AvailabilityZone),
+				string(*instance.InstanceId),
+				string(*instance.InstanceType),
+				string(instance.LaunchTime.Format("2006-01-02 15:04:05")),
+			})
+		}
+	}
+	return tbl, tblHeaders
 }
 
 func main() {
@@ -28,19 +85,9 @@ func main() {
 	// Create new EC2 client
 	svc := ec2.New(sess)
 
-	// Define search params
-	params := &ec2.DescribeInstancesInput{
-		Filters: []*ec2.Filter{
-			&ec2.Filter{
-				Name: aws.String("tag:Name"),
-				Values: []*string{
-					aws.String("*" + cli.Search + "*"),
-				},
-			},
-		},
-	}
+	filter := buildSearchFilter(cli.FilterType)
 
-	result, err := svc.DescribeInstances(params)
+	result, err := svc.DescribeInstances(filter)
 
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
@@ -61,33 +108,14 @@ func main() {
 		return
 	}
 
-	tableData := [][]string{}
-
-	for _, reservation := range result.Reservations {
-		for _, instance := range reservation.Instances {
-			var nameTag string
-			for _, t := range instance.Tags {
-				if *t.Key == "Name" {
-					nameTag = *t.Value
-					break
-				}
-			}
-
-			tableData = append(tableData, []string{
-				string(nameTag),
-				string(*instance.PrivateIpAddress),
-				string(*instance.State.Name),
-				string(*instance.Placement.AvailabilityZone),
-				string(*instance.InstanceId),
-				string(*instance.InstanceType),
-				string(instance.LaunchTime.Format("2006-01-02 15:04:05")),
-			})
-		}
+	if cli.IpOnly {
+		fmt.Println(strings.Join(buildPrivateIpData(result)[:], cli.Delimiter))
+	} else {
+		tableData, tableHeaders := buildTableData(result)
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader(tableHeaders)
+		table.SetAutoFormatHeaders(true)
+		table.AppendBulk(tableData)
+		table.Render()
 	}
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"Name", "PrivateIp", "State", "AZ", "InstanceId", "InstanceType", "LaunchTime"})
-	table.SetAutoFormatHeaders(true)
-	table.AppendBulk(tableData)
-	table.Render()
 }
